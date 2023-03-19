6384c229e87c37df3f02f4ae6a0a106d882c451d
Stefan Frank <stefanfrankbio@gmail.com>
2023-03-19 08:53:54 +0100

    removed obsolete code

--- scripts/report_table.py
+++ scripts/report_table.py
@@ -1,19 +1,5 @@
 import argparse
 import pandas as pd
-# def main():
-#     df1 = pd.read_csv('data/reports/meme_report.tsv', sep='\t', header=None, names=['gene', 'meme'])
-#     df2 = pd.read_csv('data/reports/fel_report.tsv', sep='\t', header=None, names=['gene', 'fel'])
-#     df3 = pd.read_csv('data/reports/slac_report.tsv', sep='\t', header=None, names=['gene', 'slac'])
-#     df4 = pd.read_csv('data/reports/cml_report.tsv', sep='\t', header=None, names=['gene', 'codeml'])
-#     df5 = pd.read_csv('data/reports/abritamr_report.tsv', sep='\t', header=None, names=['gene', 'abritamr'])
-#     df_merged = pd.merge(df1, df2, on='gene', how='outer')
-#     df_merged = pd.merge(df_merged, df3, on='gene', how='outer')
-#     df_merged = pd.merge(df_merged, df4, on='gene', how='outer')
-#     df_merged = pd.merge(df_merged, df5, on='gene', how='outer')
-#     df_merged["abritamr"].fillna('BASE', inplace=True)
-#     df_merged.to_csv('data/reports/report_table.tsv', sep='\t', index=False)
-#     df_no_NaN = df_merged.dropna()
-#     df_no_NaN.to_csv('data/reports/report_table_no_NaN.tsv', sep='\t', index=False)
 
 def parse_args():
     parser = argparse.ArgumentParser()
