1283fc24aa99fdd9e3eaa8abc5395c529f1df019
Stefan Frank <stefanfrankbio@gmail.com>
2023-03-20 21:08:56 +0100

    no report for min, max and stdev

--- scripts/dNdS_report.py
+++ scripts/dNdS_report.py
@@ -1,16 +1,29 @@
+import argparse
+import numpy as np
 import pandas as pd
-import glob
 
-filenames = glob.glob('data/dNdS/*.dnds')
-out_df = pd.DataFrame([], columns=['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
-genes = []
-for file in filenames:
-    genes.append(file.split('/')[-1].split('.')[0])
-    df = pd.read_table(file, sep='\t', header=0)
-    filtered_df = df.query("status == 'passed'")['dN/dS']
-    out_df.loc[len(out_df)] = filtered_df.describe()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input")
+    parser.add_argument("-g", "--gene")
+    return parser.parse_args()
 
-out_df.insert(0, 'gene', genes)
-out_df.sort_values(by='mean', ascending=False, inplace=True)
+def parse_file(file_path):
+    df = pd.read_csv(file_path, sep='\t')
+    dNdS_values = df['dNdS'].replace('divZero', np.nan).dropna().astype(float)
+    return dNdS_values.tolist()
 
-out_df.to_csv('dNdS.report', sep='\t', index=False)
+def main():
+    args = parse_args()
+    input_file = args.input
+    dNdS_values = parse_file(input_file)
+    
+    if len(dNdS_values) > 0:
+        # min_val = min(dNdS_values)
+        mean_val = np.mean(dNdS_values)
+        # max_val = max(dNdS_values)
+        # std_dev = np.std(dNdS_values)
+        print(f"{args.gene}\t{mean_val}")
+
+if __name__ == "__main__":
+    main()
